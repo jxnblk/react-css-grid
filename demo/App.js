01eb9261c25e3cdb4b92a633e2adac0bc85b9dcb
jxnblk <jacksonblack@gmail.com>
2016-07-31 13:07:15 -0400

    Edit demo

--- demo/App.js
+++ demo/App.js
@@ -5,9 +5,12 @@ import Grid from '../src/Grid'
 class App extends React.Component {
   render () {
     return (
-      <div className=''>
-        <h1 className='p3'>React CSS Grid</h1>
-        <div>
+      <div>
+        <header className='p3'>
+          <h1>React CSS Grid Demo</h1>
+          <a href='https://github.com/jxnblk/react-css-grid'>GitHub</a>
+        </header>
+        <div className='debug'>
           <Grid className='p3' sm={6} md={3}>
             <h2 className='m0'>sm6 md3</h2>
           </Grid>
